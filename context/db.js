dcbcd92d9d9a4a91da13efbe78c101f1971c1fb1
Alex <xvoroctenk@mail.ru>
2017-05-19 20:47:42 +0300

    test

--- context/db.js
+++ context/db.js
@@ -4,9 +4,9 @@ global.isProduction
 
 module.exports = (Sequelize, config) => {
     const options = {
-        host: config.db.host,
-        dialect: 'postgres',
-        port: 5432,
+        host: isProduction ? config.db.host : config.dbtest.host,
+        dialect: isProduction ? 'postgres' : 'mysql',
+        port: isProduction ? 5432 : 3306,
         logging: true,
         define: {
             timestamps: true,
